3c890376b04b4364bf0cb8616f0685e70170d495
noexanu <solonovichyan@gmail.com>
2021-04-18 19:29:28 +0200

    feat: add saveFile method and input->output pipe

--- fileConverter.js
+++ fileConverter.js
@@ -5,35 +5,49 @@
     Требования: стиль ООП, тесты(unit and integration tests), git, обработка всех исключений.
 */
 
+
+
 const fileSystem = require('fs');
 
 class File {
-
-  #name
-  #extantion
-  #dirrectory
-
-  setName(name) { this.#name = name }
-  setExt(extantion) { this.#extantion = extantion }
-  setDirr(dirrectory) { this.#dirrectory = dirrectory }
-  setParams(dirrectory, name, extantion) {
-    this.#name = name;
-    this.#extantion = extantion;
-    this.#dirrectory = dirrectory;
+  constructor(directory, name, extension) {
+    this.name = name;
+    this.extension = extension;
+    this.directory = directory;
   }
-
-  readFile() {
-    const readStream = fileSystem.createReadStream(`${this.#dirrectory}${this.#name}.${this.#extantion}`);
-    readStream.on('data', (data) => {
-      console.log(data.toString());
-      readStream.pause();
-      setTimeout(() => readStream.resume(), 1000);
-    });
-    readStream.on('error', err => { throw new Error(err) });
+  #extensions = ['txt','xml','json','csv']
+  #readFile = function() {
+    const
+      link = `${this.directory}${this.name}.${this.extension}`,
+      readStream = fileSystem.createReadStream(link);
+    readStream.on('error', err => { throw err });
+    return readStream;
   }
+  #saveFile = function() {
+    const 
+      link = `${this.directory}${this.name}.${this.extension}`,
+      writeStream = fileSystem.createWriteStream(link);
+    writeStream.on('error', err => { throw err });
+    return writeStream;
+  }
+  #isSupported = function() { 
+    return this.#extensions.includes(this.extension) ? true : false;
+  }
+  convertTo(outputFile) {
+    const inputFile = this;
+    if (inputFile.#isSupported() && outputFile.#isSupported()) {
 
+
+      
+      inputFile.#readFile().pipe(outputFile.#saveFile());
+    }
+    else { throw new Error('Files have unsupported extensions. Converter works only with .xml, .json and .csv extensions') }
+  }
 }
 
-const inputFile = new File();
-inputFile.setParams('../','pi','txt');
-inputFile.readFile();
+
+
+const
+  inputFile = new File('../','pi','txt'),
+  outputFile = new File('../','piCopy','txt');
+inputFile.convertTo(outputFile);
