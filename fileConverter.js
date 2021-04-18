9462f8b3f5ee0843fedee534d73b4572be5eafe8
noexanu <solonovichyan@gmail.com>
2021-04-18 15:06:14 +0200

    feat: add File class

--- fileConverter.js
+++ fileConverter.js
@@ -4,3 +4,36 @@
     Использование: fileConverter c:\...\input.xml d:\...\output.csv.
     Требования: стиль ООП, тесты(unit and integration tests), git, обработка всех исключений.
 */
+
+const fileSystem = require('fs');
+
+class File {
+
+  #name
+  #extantion
+  #dirrectory
+
+  setName(name) { this.#name = name }
+  setExt(extantion) { this.#extantion = extantion }
+  setDirr(dirrectory) { this.#dirrectory = dirrectory }
+  setParams(dirrectory, name, extantion) {
+    this.#name = name;
+    this.#extantion = extantion;
+    this.#dirrectory = dirrectory;
+  }
+
+  readFile() {
+    const readStream = fileSystem.createReadStream(`${this.#dirrectory}${this.#name}.${this.#extantion}`);
+    readStream.on('data', (data) => {
+      console.log(data.toString());
+      readStream.pause();
+      setTimeout(() => readStream.resume(), 1000);
+    });
+    readStream.on('error', err => { throw new Error(err) });
+  }
+
+}
+
+const inputFile = new File();
+inputFile.setParams('../','pi','txt');
+inputFile.readFile();
