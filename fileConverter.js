247d583fcc0a8e5bcfaab6336e658a53043b2393
noexanu <solonovichyan@gmail.com>
2021-04-18 12:44:44 +0200

    init: fileConverter project

---
+++ fileConverter.js
@@ -0,0 +1,6 @@
+/*
+    fileConverter - CLI tool. 
+    Поддерживаемые типы файлов: .xml, .json, .csv.
+    Использование: fileConverter c:\...\input.xml d:\...\output.csv.
+    Требования: стиль ООП, тесты(unit and integration tests), git, обработка всех исключений.
+*/
