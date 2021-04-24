93fff16ccd81f9c24e8bdc636c1da5293f591397
noexanu <solonovichyan@gmail.com>
2021-04-24 21:21:07 +0200

    feat: add input parser (test)

--- fileConverter.js
+++ fileConverter.js
@@ -4,3 +4,19 @@
     Использование: fileConverter c:\...\input.xml d:\...\output.csv.
     Требования: стиль ООП, тесты(unit and integration tests), git, обработка всех исключений.
 */
+
+
+
+if (process.argv.length < 4) { throw new Error('You should specify 2 directories. Example: c:/.../input.json d:/.../output.xml') };
+const arguments = process.argv.slice(2);
+const link1 = {
+  location : arguments[0].match(/.+\\/)[0],
+  name : arguments[0].match(/[^\\]+(?=\.)(?!.+\\)/)[0],
+  extension : arguments[0].match(/\w+$/)[0]
+
+  // location : /.+(?=\\)/.match(arguments[0]),
+  // name : /[^\\]+(?=\.)(?!.+\\)/.match(arguments[0]),
+  // extension : /\w+$/.match(arguments[0])
+}
+const link2 = arguments[1]
+console.log(link1, link2)
